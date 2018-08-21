f15a9252ee5ff52195b0569a92d69243dabb3213
Brendan Thomas <bt331609@gmail.com>
2018-08-20 20:52:53 -0400

    it's pretty much working! the confirmation needs fixing though

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,6 +1,14 @@
 class PlayersController < ApplicationController
   def index
     @players = Player.all.order(:rank)
-    render
+    render 'index'
+  end
+
+  def update
+    @player = Player.find(params[:id])
+    @player.update drafted: !@player.drafted
+    respond_to do |format|
+      format.js
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :players, only: :index
+  root to: 'players#index'
+  resources :players, only: [:index, :update]
 end
